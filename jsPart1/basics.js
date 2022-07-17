bfd0078e4fab2e05586791e983f02c857b44e9c7
tzakrzewski <ted.zakrzewski@ucdavis.edu>
2022-07-17 16:37:53 -0500

    Adding Basics+ControlFlow

---
+++ jsPart1/basics.js
@@ -0,0 +1,39 @@
+let name;
+//Modern best practice is to have them in their own line but possible
+let first, last = 'Hello';
+
+//Constant
+const interestRate = 0.3;
+
+//Object Literal {}
+let person = {
+  name: 'Mosh',
+  age: 30
+};
+
+//Accessing Members
+person.name = 'Teddy';
+person['name'] = 'Teddy'; // Best for when user selectes property at runtime
+
+//Array Literal []
+let selectedColor = ['red', 'blue'];
+
+//Accessing Members
+selectedColor[0] = 'purple';
+
+//Function
+let test = function greet(name) { //Note it doesn't have let/variable type
+  //Body
+  return name; // Note type isn't declared for function
+}
+
+//EXERCISE
+//Swap a and b
+let a = 'red';
+let b = 'blue';
+
+let c = a;
+a = b;
+b= c;
+
+//DONE
